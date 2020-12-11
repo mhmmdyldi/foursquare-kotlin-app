6ed297b363454b46286dc70c0963e47fa63831a3
mohammadyaldaie <myrises502@gmail.com>
2020-12-12 02:57:44 +0330

    First commit

---
+++ app/src/main/java/com/mhmmd/baladfoursquare/ui/nearestLocations/view/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.mhmmd.baladfoursquare.ui.nearestLocations.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mhmmd.baladfoursquare.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
